38ecbeb11c3267dfdbe23b5f82985429fc5c88ac
jondavidford <jondavidford@gmail.com>
2015-02-25 01:13:37 -0600

    print a collection of ints fix bug 5

--- railgun.rkt
+++ railgun.rkt
@@ -190,7 +190,7 @@ map~a<<<dimGrid, dimBlock>>>(~a, ~a);
         (format #<<--
 ~a
 
-int __collection_size = ~a->size;
+int __collection_size = ~a->count;
 printf("[");
 for (int i = 0; i < __collection_size-1; ++i) {
     printf("%d ,", ~a->elements[i]);
